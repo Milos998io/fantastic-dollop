a07288f690d4fab056e74d2b13d9d79de735a489
Milos <milos@Miloss-Laptop.local>
2025-04-20 00:05:12 +0200

    initial push

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("milos");
+
+// Kreiranje novog <p> elementa
+const noviElement = document.createElement("p");
+
+// Dodavanje teksta unutar elementa
+noviElement.textContent = "Miloš test radi";
+
+// Dodavanje elementa u body stranice
+document.body.appendChild(noviElement);
